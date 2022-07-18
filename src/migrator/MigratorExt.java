7d26ff2dcee1dd6dd0cb2ddaf147ea91a25483d4
Deepanshu Shah <shahdeepanshu90@gmail.com>
2022-07-17 20:59:06 -0400

    Adding data in mongodb database

--- src/migrator/MigratorExt.java
+++ src/migrator/MigratorExt.java
@@ -1,10 +1,14 @@
 package migrator;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 import metadata.DBPropertiesObj;
 import metadata.MetadataParser;
-
+import migrator.MongoDBMigrator;
 public class MigratorExt {
 
 	public static void main(String[] args) {
@@ -12,6 +16,8 @@ public class MigratorExt {
 		try {
 			JSONObject metadata = MetadataParser.parseMetadata(input);
 			DBPropertiesObj dbp = new DBPropertiesObj(metadata);
+
+	    
 		} catch (Exception ex) {
 			System.out.println(ex.toString());
 		}
--- src/migrator/MongoDBMigrator.java
+++ src/migrator/MongoDBMigrator.java
@@ -1,12 +1,45 @@
 package migrator;
 
+import org.bson.Document;
 import org.json.JSONObject;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import org.json.JSONArray;
+
+import java.util.Iterator;
 
 public class MongoDBMigrator {
 
-	public void insertData(JSONObject metadata, JSONObject data) {
-		// convert JSON to BSON
-		// connect to MongoDB
-		// insert data
+	public void insertData(JSONObject metadata,JSONArray array){
+		JSONArray arr = new JSONArray();
+        for (int i = 0; i < 10; i++) {
+            JSONObject object = new JSONObject();
+            object.put("key1" + i, i);
+            object.put("key2" + i, 1.0 + i);
+            object.put("key3" + i, "lol" + i);
+            arr.put(object);
+        }
+        
+		MongoClient client = MongoClients.create("mongodb://localhost:27017");
+		MongoDatabase database = client.getDatabase("SampleData");
+        MongoCollection<Document> collection = database.getCollection("SampleCollection");
+
+        for (int i = 0; i < arr.length(); i++) {
+            Document document = new Document();
+            JSONObject temp = arr.getJSONObject(i);
+            Iterator iterator = temp.keys();
+            while (iterator.hasNext()) {
+                Object obj = iterator.next();
+                System.out.println(obj + ": " + temp.get(obj.toString()));
+                document.append(obj.toString(), temp.get(obj.toString())); // you add this into the collection using collection.insert(basicDBObject)
+            }
+            collection.insertOne(document);
+        }
+        client.close();
 	}
 }
+
+
