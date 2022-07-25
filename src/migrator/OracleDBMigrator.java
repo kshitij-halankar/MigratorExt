c6dc173936962fcdf911d18c2e596e0ed23c707e
kshitij-halankar <halanka@uwindsor.ca>
2022-07-25 17:05:30 -0400

    data type changes for mongo to oracle

--- src/migrator/OracleDBMigrator.java
+++ src/migrator/OracleDBMigrator.java
@@ -3,7 +3,10 @@ package migrator;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -20,7 +23,7 @@ import utils.Constants;
 public class OracleDBMigrator {
 
 	public int[] insertCSVData(JSONObject metadata, JSONObject entity, CSVReader csvReader, String sql,
-			List<Integer> columnNumber) throws SQLException, CsvValidationException, IOException {
+			List<Integer> columnNumber, String tableName) throws SQLException, CsvValidationException, IOException {
 		// convert CSV to SQL
 		String[] row = null;
 
@@ -30,6 +33,14 @@ public class OracleDBMigrator {
 
 		OracleDBConnector oracleDBConnector = new OracleDBConnector();
 		Connection conn = oracleDBConnector.getConnection(dbURL, dbUserName, dbPassword);
+		ResultSetMetaData tableMetadata = getTableMetadata(metadata, tableName);
+
+//		System.out.println("tableMetadata: "+tableMetadata.getColumnCount());
+		JSONObject dataTypes = new JSONObject();
+		for (int k = 1; k <= tableMetadata.getColumnCount(); k++) {
+//			System.out.println("tableMetadata: "+tableMetadata.getColumnName(k)+" : "+tableMetadata.getColumnTypeName(k));
+			dataTypes.put(tableMetadata.getColumnName(k), tableMetadata.getColumnTypeName(k));
+		}
 		PreparedStatement p = conn.prepareStatement(sql);
 		int batchSize = 0;
 		int[] insertResult = null;
@@ -72,33 +83,51 @@ public class OracleDBMigrator {
 		JSONArray entities = metadata.getJSONObject(Constants.SCHEMA).getJSONArray(Constants.ENTITIES);
 
 		JSONArray mappings = null;
-		for (int i = 0; i < entities.length(); i++) {
-			if (entities.getJSONObject(i).getString(Constants.INPUT_ENTITY_NAME).equals(entity)) {
-				mappings = entities.getJSONObject(i).getJSONArray(Constants.MAPPINGS);
+		for (int m = 0; m < entities.length(); m++) {
+			if (entities.getJSONObject(m).getString(Constants.INPUT_ENTITY_NAME).equals(entity)) {
+				mappings = entities.getJSONObject(m).getJSONArray(Constants.MAPPINGS);
 			}
-		}
+			String tableName=entities.getJSONObject(m).getString(Constants.OUTPUT_ENTITY_NAME);
+			ResultSetMetaData tableMetadata = getTableMetadata(metadata, tableName);
+
+//		System.out.println("tableMetadata: "+tableMetadata.getColumnCount());
+			JSONObject dataTypes = new JSONObject();
+			for (int k = 1; k <= tableMetadata.getColumnCount(); k++) {
+//			System.out.println("tableMetadata: "+tableMetadata.getColumnName(k)+" : "+tableMetadata.getColumnTypeName(k));
+				dataTypes.put(tableMetadata.getColumnName(k), tableMetadata.getColumnTypeName(k));
+			}
+			System.out.println(dataTypes);
 //        JSONObject mappingAttributes = new JSONObject();
 //        for (int k = 0; k < mappings.length(); k++) {
 //            mappingAttributes.put(mappings.getJSONObject(k).getString(Constants.INPUT_ATTRIBUTE_NAME),
 //                    mappings.getJSONObject(k).getString(Constants.OUTPUT_ATTRIBUTE_NAME));
 //        }
-		for (int i = 0; i < dataArray.length(); i++) {
-			JSONObject dataRow = dataArray.getJSONObject(i);
-			for (int j = 1; j <= mappingAttributes.size(); j++) {
-				for (int k = 0; k < mappings.length(); k++) {
+			for (int i = 0; i < dataArray.length(); i++) {
+				JSONObject dataRow = dataArray.getJSONObject(i);
+				for (int j = 1; j <= mappingAttributes.size(); j++) {
+					for (int k = 0; k < mappings.length(); k++) {
 //                    System.out.println("test "+mappings.getJSONObject(k).getString(Constants.INPUT_ATTRIBUTE_NAME));
-					if (mappingAttributes.get(j - 1)
-							.equals(mappings.getJSONObject(k).getString(Constants.OUTPUT_ATTRIBUTE_NAME))) {
+						if (mappingAttributes.get(j - 1)
+								.equals(mappings.getJSONObject(k).getString(Constants.OUTPUT_ATTRIBUTE_NAME))) {
 //                        System.out.println("mapping: " + mappingAttributes.get(j - 1) + " - "+ mappings.getJSONObject(k).getString(Constants.INPUT_ATTRIBUTE_NAME));
-						p.setString(j,
-								dataRow.get(mappings.getJSONObject(k).get(Constants.INPUT_ATTRIBUTE_NAME).toString()).toString());
+							String attributeName = mappings.getJSONObject(k).getString(Constants.OUTPUT_ATTRIBUTE_NAME);
+							String dataType = dataTypes.getString(attributeName.toUpperCase());
+							if (dataType == "NUMBER") {
+								p.setInt(j, dataRow
+										.getInt(mappings.getJSONObject(k).getString(Constants.INPUT_ATTRIBUTE_NAME)));
+							} else {
+								p.setString(j, dataRow
+										.get(mappings.getJSONObject(k).get(Constants.INPUT_ATTRIBUTE_NAME).toString())
+										.toString());
+							}
+
 //                        System.out.println("dataRow: " + dataRow.getString(mappings.getJSONObject(k).getString(Constants.INPUT_ATTRIBUTE_NAME)));
-						break;
-					}
+							break;
+						}
 //                    System.out.println(p.toString());
+					}
+					System.out.println();
 				}
-				System.out.println();
-			}
 //            JSONObject insertRow = new JSONObject();
 //            Iterator<String> dataRowKeys = dataRow.keys();
 //            int cols = 1;
@@ -111,16 +140,17 @@ public class OracleDBMigrator {
 //                }
 //            }
 
-			p.addBatch();
-			p.clearParameters();
-			batchSize++;
-			if (batchSize == Constants.BATCH_SIZE) {
-				insertResult = p.executeBatch();
-				batchSize = 0;
+				p.addBatch();
+				p.clearParameters();
+				batchSize++;
+				if (batchSize == Constants.BATCH_SIZE) {
+					insertResult = p.executeBatch();
+					batchSize = 0;
+				}
 			}
-		}
-		if (batchSize > 0) {
-			insertResult = p.executeBatch();
+			if (batchSize > 0) {
+				insertResult = p.executeBatch();
+			}
 		}
 		return insertResult;
 	}
@@ -144,5 +174,19 @@ public class OracleDBMigrator {
 		}
 
 		return insertResult;
+	}
+
+	public ResultSetMetaData getTableMetadata(JSONObject metadata, String entityName) throws SQLException {
+		ResultSetMetaData tableMetaData = null;
+		String dbURL = metadata.get(Constants.OUTPUT_SOURCE).toString();
+		String dbUserName = metadata.get(Constants.OUTPUT_SOURCE_LOGIN_USERNAME).toString();
+		String dbPassword = metadata.get(Constants.OUTPUT_SOURCE_LOGIN_PASSWORD).toString();
+		OracleDBConnector oracleDBConnector = new OracleDBConnector();
+		Connection con = oracleDBConnector.getConnection(dbURL, dbUserName, dbPassword);
+		Statement stmt = con.createStatement();
+		System.out.println(entityName);
+		ResultSet rs = stmt.executeQuery("select * from " + entityName + " where ROWNUM <= 1");
+		tableMetaData = rs.getMetaData();
+		return tableMetaData;
 	}
 }
